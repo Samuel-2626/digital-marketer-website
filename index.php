756362a892a94bbaa82a169e6e8531265339feb7
Samuel Torimiro <efosonaname@gmail.com>
2023-05-02 11:33:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,206 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bruno+Ace+SC&family=Roboto+Slab&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <meta name="author" content="Adeleke Toluwalope">
+    <meta name="description" content="I am a passionate digital marketer. I help businesses leverage
+            social media and search platforms to win new customers, generate quality leads and grow their
+            presence through effective marketing strategies, thereby increasing their Return on Investment (ROI)">
+    <meta name="keywords" content="Adeleke Toluwalope, Digital Marketer, Social Media Marketing/Management, Paid Advertising, Google Analytics, SEO, Conceptualization & Copywriting, 1-on-1 Consultation">
+     <meta name="robots" content="index, follow">
+    <title>Adeleke Toluwalope | Digital Marketer</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light" id="navbar-example2">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="images/logo.png" alt="" width="30" height="24"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#about-me">About Me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#certificates">Certificates</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#testimonials">Testimonials</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#connect">Contact</a>
+        </li>
+      </ul>
+ 
+    </div>
+  </div>
+</nav>
+<main>
+<div class="p-5 pb-0 rounded-3">
+      <div class="container py-3">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="images/main.jpg" alt="Profile Image" class="main__img img-thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h1>Hello there!</h1>
+            <p>
+            My name is Adeleke Toluwalope. I am a passionate digital marketer. I help businesses leverage
+            social media and search platforms to win new customers, generate quality leads and grow their
+            presence through effective marketing strategies, thereby increasing their <strong>Return on Investment (ROI)</strong>.
+
+            </p>     
+<br>
+<a href="mailto:info.tolusocials@gmail.com" class="btn btn-lg mb-4">Work with me?</a> &nbsp; &nbsp; &nbsp; &nbsp;
+<a href="download.php?file=Adeleke Toluwalope (Resume).pdf" class="btn btn-lg mb-4">Download Resume <i class="fa fa-download"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div data-target="#navbar-example2" data-spy="scroll" id="about-me">
+    <div class="container">
+    <div class="about pt-4 p-2">
+      <h2 class="text-center pb-2">About Me</h2>
+      <ul>
+      <li>Seasoned professional with experience in advertising</a></li>
+        <li>I help businesses grow their online presence</li>
+        <li>I'm capable of implementing effective marketing strategies for brands</li>
+        <li>I offer digital marketing solutions to businesses of all sizes, regardless of niche, industry, or geographical location</li>
+        <li>I write compelling copy that attracts potential customers/clients</li>
+      </ul>
+    </div>
+    </div>
+    </div>
+
+    <div data-target="#navbar-example2" data-spy="scroll" id="services">
+
+    <div class="container projects">
+      <div class="mt-5">
+        <h2 class="text-center">Projects I work On</h2>
+        <br>
+        <div class="row">
+        <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-cogs fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">Social Media Marketing/Management</h4>
+
+
+                    </div>
+            <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-bullhorn fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">Paid Advertising</h4>
+
+            </div>
+            <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-line-chart fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">Google Analytics</h4>
+
+            </div>
+            <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-balance-scale fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">SEO</h4>
+
+            </div>
+
+            <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-book fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">Conceptualization & Copywriting</h4>
+
+           </div>
+
+           <div class="col-md-4 col-sm-6 hover--effect pb-5">
+                <p class="text-center no--box">
+                    <i class="fa fa-handshake-o fa-5x" style="color: #004aad;"></i>
+                </p>
+                <h4 class="text-center">1-on-1 Consultation</h4>
+
+           </div>
+
+        </div>
+      </div>
+      <hr>
+    <a href="mailto:info.tolusocials@gmail.com" class="btn btn-lg mb-4" style="display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 200px;">Work with me?</a>
+    </div>
+    
+    </div>
+
+    <br>
+
+    <div data-target="#navbar-example2" data-spy="scroll" id="certificates">
+    <div class="container">
+    <div class="certificates pt-4 p-2">
+
+    <h2 class="text-center pb-2">Certificates</h2>
+    <!-- <p class="text-center">Tech content that resonates with developers</p> -->
+    <div class="row">
+      <div class="col-md-4 pb-3"><img src="images/google-ads.png" alt="Google Ads" class="img-fluid img-thumbnail"></div>
+      <div class="col-md-4 pb-3"><img src="images/social-media-manager.jpg" alt="Social Media Manager" class="img-fluid img-thumbnail"></div>
+      <div class="col-md-4 pb-3"><img src="images/digital-marketing-foundations.jpg" alt="Digital Marketing Foundations" class="img-fluid img-thumbnail"></div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <br>
+    <div class="container">
+    <hr>
+    <br>
+    <div data-target="#navbar-example2" data-spy="scroll" id="connect">
+      
+
+      
+    <div class="connect text-center">
+      <h4>Connect</h4>
+      <br>
+      <a href="mailto:info.tolusocials@gmail.com" class="m-3"><i class="fa fa-envelope-o" style="font-size:36px; color: #004aad;"></i></a>
+      <a href="https://twitter.com/tolu_socials" class="m-3"><i class="fa fa-twitter" style="font-size:36px; color: #004aad;"></i></a>
+      <a href="https://www.facebook.com/profile.php?id=100090678185058" class="m-3"><i class="fa fa-facebook" style="font-size:36px; color: #004aad;"></i></a>
+      <a href="https://www.instagram.com/tolu_socials/ class="m-3"><i class="fa fa-instagram" style="font-size:36px; color: #004aad;"></i></a>
+    </div>
+    </div>
+    </div>
+
+    <br>
+    
+
+</main>
+
+<br>
+
+<footer>
+  <p class="text-center">Copyright &copy; 2023 Adeleke Toluwalope. All Rights Reserved</p>
+</footer>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
